c7be47e5dde6f4650380bc0286f419efe30bb6b8
MrXploder <l.arancibiaf@gmail.com>
2019-03-01 23:49:11 -0300

    backup

--- uxRelay.ino
+++ uxRelay.ino
@@ -7,18 +7,29 @@
 ESP8266WebServer server(80);
 ESP8266WiFiMulti wifiMulti;
 
-
-const char *ssid = "Jumpitt Labs";
-const char *password = "Jumpitt2015";
 const char *mdnsName = "uxrelay1";
 
 bool state = false;
+
+// constants won't change. Used here to set a pin number:
+const int ledPin =  LED_BUILTIN;// the number of the LED pin
+
+// Variables will change:
+int ledState = LOW;             // ledState used to set the LED
+
+// Generally, you should use "unsigned long" for variables that hold time
+// The value will quickly become too large for an int to store
+unsigned long previousMillis = 0;        // will store last time LED was updated
+
+// constants won't change:
+const long interval = 1000;           // interval at which to blink (milliseconds)
 
 void setup() {
   Serial.begin(115200);
   delay(10);
   Serial.println("\r\n");
 
+  pinMode(ledPin, OUTPUT);
   pinMode(D1, OUTPUT);
 
   startWiFi();
@@ -27,7 +38,24 @@ void setup() {
 }
 
 void loop() {
+  unsigned long currentMillis = millis();
+
   server.handleClient();
+
+  if (currentMillis - previousMillis >= interval) {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
+
+    // if the LED is off turn it on and vice-versa:
+    if (ledState == LOW) {
+      ledState = HIGH;
+    } else {
+      ledState = LOW;
+    }
+
+    // set the LED with the ledState of the variable:
+    digitalWrite(ledPin, ledState);
+  }
 }
 
 void startWiFi() {
